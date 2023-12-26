9cca5adbef95922b2e321ddb8160ed1a77c51396
Issei-Sato-0926 <satoon.0926@outlook.jp>
2023-12-26 13:01:25 +0900

    リストと例外処理を用いたサンプルプログラムを作成

---
+++ src/main/java/com/sato/Main.java
@@ -0,0 +1,27 @@
+package com.sato;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("佐藤");
+        names.add("鈴木");
+        names.add("田中");
+        names.add("高橋");
+
+        System.out.println("要素の数は" + names.size() + "です");
+        System.out.println("0番目の要素を表示：" + names.get(0));
+        System.out.println("1番目の要素を表示：" + names.get(1));
+        System.out.println("2番目の要素を表示：" + names.get(2));
+        System.out.println("3番目の要素を表示：" + names.get(3));
+
+        System.out.println("-------------------------------");
+        try {
+            System.out.println("4番目の要素を表示：" + names.get(4));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("要素が存在しません");
+        }
+    }
+}
