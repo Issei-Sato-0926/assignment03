d577bf0c918f506e768ff94be0ed714ca78c6ac9
Issei-Sato-0926 <satoon.0926@outlook.jp>
2023-12-27 23:55:05 +0900

    リストに山田を追加

--- src/main/java/com/sato/Main.java
+++ src/main/java/com/sato/Main.java
@@ -10,15 +10,17 @@ public class Main {
         names.add("鈴木");
         names.add("田中");
         names.add("高橋");
+        names.add("山田");
 
         System.out.println("要素の数は" + names.size() + "です");
         System.out.println("0番目の要素を表示：" + names.get(0));
         System.out.println("1番目の要素を表示：" + names.get(1));
         System.out.println("2番目の要素を表示：" + names.get(2));
         System.out.println("3番目の要素を表示：" + names.get(3));
+        System.out.println("4番目の要素を表示：" + names.get(4));
 
         try {
-            System.out.println("4番目の要素を表示：" + names.get(4));
+            System.out.println("4番目の要素を表示：" + names.get(5));
         } catch (IndexOutOfBoundsException e) {
             System.out.println("要素が存在しません");
         }
